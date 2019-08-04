3167890753cd44b252b38fb83d026a4d84839f75
Jason Meno <jasonmeno@msn.com>
2019-08-04 19:05:15 -0400

    Add webpage

---
+++ docs/index_files/index.js
@@ -0,0 +1,19 @@
+/**************************************
+
+ 
+ You're being sneaky again.
+ I must reward you for your thorough cleverness.
+ 
+ A hint:
+ 
+ Use the background with one of my github repositories.
+ 
+***************************************/
+
+//This fades in the welcome screen text
+$(document).ready(function(){
+	
+	$('#welcome').delay(1000).fadeIn(7000);
+	$('#construction').delay(2500).fadeIn(5000);
+					
+}); // end of $(document).ready()
